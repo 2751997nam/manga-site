73e8a828d34767f4f7f0c568e68dca02996b5ba6
nam <nambcvt275@gmail.com>
2022-02-01 22:46:10 +0700

    fix: tracking ip

--- pages/api/tracking.js
+++ pages/api/tracking.js
@@ -3,8 +3,8 @@ import DB from '@/lib/db';
 const post = async (request, response) => {
     if (request.method == "POST") {
         let params = request.body;
-        let ip = request.headers["x-real-ip"] || request.connection.remoteAddress;
-        ip = ip.replace('::ffff:', '');
+        const forwarded = request.headers["x-forwarded-for"]
+        const ip = forwarded ? forwarded.split(/, /)[0] : request.connection.remoteAddress
         const db = DB();
         const device = params.device;
         const browser = params.browser;
