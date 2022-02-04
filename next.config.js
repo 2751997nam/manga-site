46a4f88811582b60a2044e51acacff9c3242d787
nam <nambcvt275@gmail.com>
2022-02-05 01:52:53 +0700

    optimize: cdn

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     images: {
         domains: ["manhwa18.net", 'cdn.manhwa18.com', 'manhwa18.cc', 'manhwa18.com', 'manytoon.com', 'cdn4.manhwa18.com', 
             'mangagenki.com', 'i0.wp.com', 'i1.wp.com', 'i2.wp.com', 'i3.wp.com', 'i4.wp.com', 'i5.wp.com',
-            'manga18.us', 'azmin.manga18.us', 'manhuascan.us', 'localhost', 'cdn.manhwaplus.net'
+            'manga18.us', 'azmin.manga18.us', 'manhuascan.us', 'localhost', 'cdn.manhwaplus.net', 'cdn.toptoon69.com', 'toptoon69.com', 'manhwaplus.net'
         ],
     },
     async rewrites() {
