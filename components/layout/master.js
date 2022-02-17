238a31aa046dd7722382a10b23568a8bb2a50251
nam <nambcvt275@gmail.com>
2022-02-17 15:46:54 +0700

    feature: remove ads

--- components/layout/master.js
+++ components/layout/master.js
@@ -106,7 +106,6 @@ function Layout(props) {
                     <i className="fa fa-angle-double-up"></i>
                 </div>
             </div>
-            <>{renderAdsScript}</>
         </Fragment>
     );
 }
