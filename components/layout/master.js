4ac71aa20434d1ba52d15b4b19d3366ac14cc5d7
Nam <nambcvt275@gmail.com>
2022-02-25 05:24:12 +0700

    optimize: ads

--- components/layout/master.js
+++ components/layout/master.js
@@ -128,7 +128,7 @@ function Layout(props) {
                     <i className="fa fa-angle-double-up"></i>
                 </div>
             </div>
-            <>{renderAdsScript2}</>
+            <>{renderAdsScript}</>
         </Fragment>
     );
 }
